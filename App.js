212114f9cb8d2cab9545b3fa6b48595e23900857
basicjohn <edmondsonj@gmail.com>
2021-07-24 11:14:38 -0700

    WIP: build components

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Route, Switch } from  "react-router-dom";
+import TopNavigation from "./components/TopNavigation";
+import Home from "./src/pages/Home";
+import Login from "./src/pages/Login";
+import Register from "./src/pages/Register";
+import Profile from "./src/pages/Profile";
+import Logout from "./src/pages/Logout";
+import About from "./src/pages/About";
+import NotFound from "./src/pages/NotFound";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <StatusBar backgroundColor="rgba(0,0,0,0.2)" barStyle="light-content" />
+        <TopNavigation />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+          <Route path="/profile" component={Profile} />
+          <Route path="/logout" component={Logout} />
+          <Route path="/about" component={About} />
+          <Route component={NotFound} />
+        </Switch>
+      </View>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
