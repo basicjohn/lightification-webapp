6f571f94c6dbde9253fab43847c3ffa60916c474
basicjohn <edmondsonj@gmail.com>
2021-07-27 10:14:30 -0700

    create Hue api calls

--- src/components/Setup2.js
+++ src/components/Setup2.js
@@ -1,4 +1,5 @@
 // Page that lists all available Lights for you to choose from.
+// import env from './../config.json'
 import React, { Component } from 'react';
 
 class Setup2 extends Component {
@@ -7,19 +8,21 @@ class Setup2 extends Component {
     this.state = {
       error: null,
       isLoaded: false,
+      hubIp: '192.168.86.168',
+      username: 'GdKr5N2NQtBnaknaysdCMHNwTojvh7wcHuMG0Yy2',
       selectedLight: null,
       lights: []
     };
   }
 
-  makeApiCall = () => {
-    fetch(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${process.env.REACT_APP_API_KEY}`)
+  getHubUsername = () => {
+    fetch(`https://${this.state.hubIp}/api/newdeveloper`)
       .then(response => response.json())
       .then(
         (jsonifiedResponse) => {
           this.setState({
             isLoaded: true,
-            lights: jsonifiedResponse.results
+            username: jsonifiedResponse.results[0].success[0].username
           });
         })
         .catch((error) => {
@@ -29,8 +32,71 @@ class Setup2 extends Component {
           });
         });
   }
+
+
+  getAllLightsInfo = () => {
+    fetch(`https://${this.state.hubIp}/api/${this.state.username}/lights`)
+      .then(response => response.json())
+      .then(
+        (jsonifiedResponse) => {
+          this.setState({
+            isLoaded: true,
+            selectedLight: jsonifiedResponse.results[0].success[0].username
+          });
+        })
+        .catch((error) => {
+          this.setState({
+            isLoaded: true,
+            error 
+          });
+        });
+  }
+
+
+  getSelectedLightInfo = () => {
+    fetch(`https://${this.state.hubIp}/api/${this.state.username}/lights`)
+      .then(response => response.json())
+      .then(
+        (jsonifiedResponse) => {
+          this.setState({
+            isLoaded: true,
+            username: jsonifiedResponse.results[0].success[0].username
+          });
+        })
+        .catch((error) => {
+          this.setState({
+            isLoaded: true,
+            error 
+          });
+        });
+  }
+
+  componentDidMount() {
+    this.getAllLightsInfo()
+  }
   render() {
-  
+    const { error, isLoaded, lights } = this.state;
+    if (error) {
+      return <React.Fragment>Error: {error.message}</React.Fragment>;
+    } else if (!isLoaded) {
+      return <React.Fragment>Loading...</React.Fragment>;
+    } else {
+      return (
+        <React.Fragment>
+          <h1>All Lights</h1>
+          <ul>
+            {lights.map((light, index) =>
+              <li key={light[index].key}>
+                <h3>{light.name}</h3>
+                <h4>{light.productname}</h4>
+                <h5>{light.type}</h5>
+                <button>Select Light</button>
+              </li>
+            )}
+          </ul>
+        </React.Fragment>
+      );
+    }
   }
 }
 
