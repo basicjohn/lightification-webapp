69d4deb3ff4fffbf17387e1689e7b1227c67d4eb
basicjohn <edmondsonj@gmail.com>
2021-07-26 19:26:25 -0700

    OAUTH is Working

--- App.js
+++ App.js
@@ -1,79 +1,31 @@
-import React, { Component } from 'react';
-import { Route, Switch } from "react-router-dom";
-import Header from "./src/components/Header";
-import Home from "./src/pages/Home";
-import Login from "./src/pages/Login";
-import Logout from "./src/pages/Logout";
-
-const About = () => (
-  <div><h1>About</h1></div>
-)
-
-const NotFound = () => (
-  <div><h1>NotFound</h1></div>
-)
-
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Header />
-        <main role="main" className="container">
-          <Switch>
-            <Route exact path='/' component={Home} />
-            <Route path='/about' component={About} />
-            <Route path='/login' component={Login} />
-            <Route path='/logout' component={Logout} />
-            <Route component={NotFound} />
-          </Switch>
-        </main>
-      </div>
-    );
-  }
-}
-
-export default App;
-
-
-// import { StatusBar } from 'expo-status-bar';
 // import React, { Component } from 'react';
-// import { StyleSheet, Text, View } from 'react-native';
-// import { Route, Switch, BrowserRouter as Router } from  "react-router-dom";
+// import { Route, Switch } from "react-router-dom";
 // import Header from "./src/components/Header";
 // import Home from "./src/pages/Home";
 // import Login from "./src/pages/Login";
-// // import Register from "./src/pages/Register";
-// import Profile from "./src/pages/Profile";
 // import Logout from "./src/pages/Logout";
-// import About from "./src/pages/About";
-// import NotFound from "./src/pages/NotFound";
+
+// const About = () => (
+//   <div><h1>About</h1></div>
+// )
+
+// const NotFound = () => (
+//   <div><h1>NotFound</h1></div>
+// )
 
 // class App extends Component {
-//   // constructor(props) {
-//   //   super(props)
-//   //   this.state = {
-//   //     // isAuthenticated: false
-//   //   }
-//   // }
-
 //   render() {
 //     return (
-//       // <View style={styles.container}>
 //       <div>
-//         {/* <StatusBar backgroundColor="rgba(0,0,0,0.2)" barStyle="light-content" /> */}
 //         <Header />
 //         <main role="main" className="container">
-//           <Router>
-//             <Switch>
-//               <Route exact path="/" component={Home} />
-//               <Route path="/about" component={About} />
-//               {/* <Route path="/register" component={Register} /> */}
-//               <Route path="/login" component={Login} />
-//               <Route path="/profile" component={Profile} />
-//               <Route path="/logout" component={Logout} />
-//               <Route component={NotFound} />
-//             </Switch>
-//           </Router>
+//           <Switch>
+//             <Route exact path='/' component={Home} />
+//             <Route path='/about' component={About} />
+//             <Route path='/login' component={Login} />
+//             <Route path='/logout' component={Logout} />
+//             <Route component={NotFound} />
+//           </Switch>
 //         </main>
 //       </div>
 //     );
@@ -81,3 +33,51 @@ export default App;
 // }
 
 // export default App;
+
+
+import { StatusBar } from 'expo-status-bar';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Route, Switch, BrowserRouter as Router } from  "react-router-dom";
+import Header from "./src/components/Header";
+import Home from "./src/pages/Home";
+import Login from "./src/pages/Login";
+// import Register from "./src/pages/Register";
+import Profile from "./src/pages/Profile";
+import Logout from "./src/pages/Logout";
+import About from "./src/pages/About";
+import NotFound from "./src/pages/NotFound";
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      // isAuthenticated: false
+    }
+  }
+
+  render() {
+    return (
+      // <View style={styles.container}>
+      <div>
+        {/* <StatusBar backgroundColor="rgba(0,0,0,0.2)" barStyle="light-content" /> */}
+        <Header />
+        <main role="main" className="container">
+          <Router>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/about" component={About} />
+              {/* <Route path="/register" component={Register} /> */}
+              <Route path="/login" component={Login} />
+              <Route path="/profile" component={Profile} />
+              <Route path="/logout" component={Logout} />
+              <Route component={NotFound} />
+            </Switch>
+          </Router>
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
--- index.js
+++ index.js
@@ -1,48 +1,45 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom';
-// import { BrowserRouter as Router } from 'react-router-dom';
-// import 'react-native-gesture-handler';
-// import { registerRootComponent } from 'expo';
-// import { Provider } from 'react-redux';
-// import { createStore, applyMiddleware } from 'redux';
-
-// import App from './App';
-// import middlewareApi from './src/middleware/middleware-api';
-// import thunkMiddleware from 'redux-thunk';
-
-// import 'bootstrap/dist/css/bootstrap.css';
-// import './index.css';
-// // import registerServiceWorker from './registerServiceWorker';
-
-// const store = createStore(
-//   // reducer,
-//   applyMiddleware(middlewareApi, thunkMiddleware)
-// )
-
-// ReactDOM.render(
-//   <Provider store={store}>
-//       <Router>
-//           <App />
-//       </Router>
-//   </Provider>,
-//   document.getElementById('root'));
-// registerRootComponent(App);
-
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { Provider } from "react-redux";
-import store from "./src/store";
-import registerServiceWorker from './src/registerServiceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
+import 'react-native-gesture-handler';
+import { registerRootComponent } from 'expo';
+import { Provider } from 'react-redux';
+import store from "./src/store";
+
+import App from './App';
+// import middlewareApi from './src/middleware/middleware-api';
+import thunkMiddleware from 'redux-thunk';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
+import registerServiceWorker from './src/registerServiceWorker';
 
 
 ReactDOM.render(
-    <Provider store={store}>
-        <Router>
-            <App />
-        </Router>
-    </Provider>,
-    document.getElementById('root'));
+  <Provider store={store}>
+      <Router>
+          <App />
+      </Router>
+  </Provider>,
+  document.getElementById('root'));
 registerServiceWorker();
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import { Provider } from "react-redux";
+// import store from "./src/store";
+// import registerServiceWorker from './src/registerServiceWorker';
+// import { BrowserRouter as Router } from 'react-router-dom';
+
+
+// ReactDOM.render(
+//     <Provider store={store}>
+//         <Router>
+//             <App />
+//         </Router>
+//     </Provider>,
+//     document.getElementById('root'));
+// registerServiceWorker();
