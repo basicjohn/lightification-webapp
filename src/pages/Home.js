03267acfc938aae6c36ea199e5f2b30843596dce
basicjohn <edmondsonj@gmail.com>
2021-07-28 08:49:15 -0700

    add copy to home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,8 +3,17 @@ import React from "react";
 function Home(props) {
 
   return(
-    <div className="container">
-      <h1>testing</h1>
+    <div>
+      <div className="jumbotron">
+        <h1>Lightification <span>w/ Philips Hue</span></h1>
+        <h2>Respect your focus using predictable minimalist notifications</h2>
+        <h3>Distract Less</h3>
+        <button>Get Started</button>
+      </div>
+      <div className="row">
+        <div className="col-md-6"></div>
+        <div className="col-md-6"></div>
+      </div>
     </div>
   );
 }
