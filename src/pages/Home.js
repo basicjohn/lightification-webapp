e71ae716421091cb75c042eb42f8af09b6843538
basicjohn <edmondsonj@gmail.com>
2021-07-28 09:06:12 -0700

    add home copy

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,8 +11,30 @@ function Home(props) {
         <button>Get Started</button>
       </div>
       <div className="row">
-        <div className="col-md-6"></div>
-        <div className="col-md-6"></div>
+        <div className="col-md-12">
+          <h4>Features</h4>
+        </div>
+        <div className="col-md-6">
+          <div className="col-md-12">
+            <img src="" alt="" />
+            <p>Connect to your Philips Hue Bridge locally or connect to the Hue website to setup remote scheduling</p>
+          </div>
+          <div className="col-md-12">
+            <img src="" alt="" />
+            <p>Connect to your Philips Hue Bridge locally or connect to the Hue website to setup remote scheduling</p>
+          </div>
+        </div>
+        <div className="col-md-6">
+          <div className="col-md-12">
+            <img src="" alt="" />
+            <p>With a variety of notification options </p>
+          </div>
+          <div className="col-md-12">
+            <img src="" alt="" />
+            <p>Connect to your Philips Hue Bridge locally or connect to the Hue website to setup remote scheduling</p>
+          </div>
+
+        </div>
       </div>
     </div>
   );
