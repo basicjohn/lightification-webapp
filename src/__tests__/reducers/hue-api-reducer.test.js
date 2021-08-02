e0fd2be356f257867ea947f0ac31baf4bf6e24c2
basicjohn <edmondsonj@gmail.com>
2021-08-02 14:22:26 -0700

    good fail of second test

--- src/__tests__/reducers/hue-api-reducer.test.js
+++ src/__tests__/reducers/hue-api-reducer.test.js
@@ -3,16 +3,45 @@ import hueApiReducer from '../../reducers/hue-api-reducer';
 describe('hueApiReducer', () => {
 
   let action;
-  const UserData = {
-    hubIp: '192.168.86.168',
+  const userData = {
+    email: 'test@test.com',
+    hubIp: '1.1.1.1',
     username: 'GdKr5N2NQtBnaknaysdCMHNwTojvh7wcHuMG0Yy2',
     selectedLight: null,
     lights: [],
     notificationType: '',
     brightness: '',
-    schedule: ''
+    schedule: '',
   };
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(hueApiReducer({}, { type: null })).toEqual({});
+  });
+
+
+  test('Should successfully add new user data to the masterUserList', () => {
+    const { email, hubIp, username, selectedLight, lights, notificationType, brightness, schedule } = userData;
+    action = {
+      type: 'ADD_USER',
+      email: email,
+      hubIp: hubIp,
+      username: username,
+      selectedLight: selectedLight,
+      lights: lights,
+      notificationType: notificationType,
+      brightness: brightness,
+      schedule: schedule
+    };
+    expect(hueApiReducer(userData, action)).toEqual({
+      [email]: {
+        email: email,
+        hubIp: hubIp,
+        username: username,
+        selectedLight: selectedLight,
+        lights: lights,
+        notificationType: notificationType,
+        brightness: brightness,
+        schedule: schedule,
+      }
+    });
   });
 });
