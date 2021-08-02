4c665cce37fa753328ac39fc6b386325448211c4
basicjohn <edmondsonj@gmail.com>
2021-08-02 13:51:46 -0700

    add sample data to test

--- src/__tests__/reducers/hue-api-reducer.test.js
+++ src/__tests__/reducers/hue-api-reducer.test.js
@@ -1,6 +1,17 @@
 import hueApiReducer from '../../reducers/hue-api-reducer';
 
 describe('hueApiReducer', () => {
+
+  let action;
+  const UserData = {
+    hubIp: '192.168.86.168',
+    username: 'GdKr5N2NQtBnaknaysdCMHNwTojvh7wcHuMG0Yy2',
+    selectedLight: null,
+    lights: [],
+    notificationType: '',
+    brightness: '',
+    schedule: ''
+  };
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(hueApiReducer({}, { type: null })).toEqual({});
   });
