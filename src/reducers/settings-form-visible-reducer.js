acd9d439498bf6fb039ce10a8b137fbff963d127
basicjohn <edmondsonj@gmail.com>
2021-08-12 13:15:09 -0700

    WIP: Form Reducer

--- src/reducers/settings-form-visible-reducer.js
+++ src/reducers/settings-form-visible-reducer.js
@@ -1,6 +1,11 @@
-function settingsFormVisibleReducer (state = {}, action) {
+function settingsFormVisibleReducer (state = false, action) {
 
-  
+  switch (action.type) {
+    case 'TOGGLE_FORM': 
+      return !state;
+    default:
+      return state;
+  }
 };
 
 export default settingsFormVisibleReducer;
