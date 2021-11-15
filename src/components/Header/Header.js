8647a7ba92e597eef493ccb78e4742ecd72f09c3
basicjohn <edmondsonj@gmail.com>
2021-11-15 13:52:29 -0800

    fix paths

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import HeaderNav from './HeaderNav';
+import HeaderNav from './HeaderNav/HeaderNav';
 import './Header.css';
 
 
